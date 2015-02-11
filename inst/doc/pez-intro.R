9cd97153bd720ad1c28c92c351fea545cba93a02
William D. Pearse <will.pearse@gmail.com>
2015-02-11 19:56:51 +0000

    version 1.0-0

--- inst/doc/pez-intro.R
+++ inst/doc/pez-intro.R
@@ -21,6 +21,8 @@ species(data)[1:2]
 species(data)[1:2] <- c("new", "names")
 sites(data)[1:2] <- c("newer", "names")
 data <- data[, colSums(data$comm) > 5]
+traits(data)$new.trait <- rep("nonsense", nrow(traits(data)))
+traits(data)$new.trait <- NULL
 
 ## ----tidy=TRUE------------------------
 shape.output <- shape(data)
@@ -59,4 +61,63 @@ fpd.data <- funct.phylo.dist(data, phyloWeight = 0.5, p = 2)
 ## ----sesFPD---------------------------
 ses.mfpd.data <- picante::ses.mpd(data$comm, fpd.data)
 head(ses.mfpd.data)[,c("ntaxa", "mpd.obs", "mpd.obs.p")]
+
+## ----<pglmmSim, tidy=TRUE-------------
+# Basic parameters
+nspp <- 15; nsite <- 10
+# Fixed effects
+beta0 <- beta1 <- 0
+# Random effects' magnitudes
+sd.B0 <- sd.B1 <- 1
+
+# Generate environmental site variable
+X <- matrix(1:nsite, nrow=1, ncol=nsite)
+X <- (X - mean(X))/sd(X)
+
+# Simulate phylogeny
+phy <- compute.brlen(rtree(nspp), method = "Grafen", power = 0.5)
+# Standardise phy. covariance matrix
+Vphy <- vcv(phy); Vphy <- Vphy/(det(Vphy)^(1/nspp))
+
+# Generate phylogenetic signal in parameters
+iD <- t(chol(Vphy))
+b0 <- beta0 + iD %*% rnorm(nspp, sd = sd.B0)
+b1 <- beta1 + iD %*% rnorm(nspp, sd = sd.B1)
+
+#Simulate presences
+y <- matrix(outer(b0, array(1, dim = c(1, nsite))), nrow = nspp, ncol = nsite) + matrix(outer(b1, X), nrow = nspp, ncol = nsite)
+e <- rnorm(nspp * nsite, sd = 0)
+y <- y + matrix(e, nrow = nspp, ncol = nsite)
+y <- matrix(y, nrow = nspp * nsite, ncol = 1)    
+Y <- rbinom(n = length(y), size = 1, prob = exp(y)/(1 + exp(y)))
+Y <- matrix(Y, nrow = nspp, ncol = nsite)
+
+#Neat up the data to show structure
+rownames(Y) <- 1:nspp; colnames(Y) <- 1:nsite
+
+## ----pglmmModel, tidy=TRUE------------
+# Transform data into 'long' format
+# - Occurrence data
+YY <- matrix(Y, nrow = nspp * nsite, ncol = 1)
+# - Environmental (site) data
+XX <- matrix(kronecker(X, matrix(1, nrow = nspp, ncol = 1)), nrow = nspp * nsite, ncol = 1)
+site <- matrix(kronecker(1:nsite, matrix(1, nrow = nspp, ncol = 1)), nrow = nspp * nsite, ncol = 1)
+sp <- matrix(kronecker(matrix(1, nrow = nsite, ncol = 1), 1:nspp), nrow = nspp * nsite, ncol = 1)
+# - Make data.frame with all data
+dat <- data.frame(Y = YY, X = XX, site = as.factor(site), sp = as.factor(sp))
+
+# Setup random effects
+# - 1: random intercept - species independent
+re.1 <- list(1, sp = dat$sp, covar = diag(nspp))
+# - 2: random intercept - species phylogenetically covary
+re.2 <- list(1, sp = dat$sp, covar = Vphy)
+# - 3: random slope - species independent
+re.3 <- list(dat$X, sp = dat$sp, covar = diag(nspp))
+# - 4: random intercept - species covary
+re.4 <- list(dat$X, sp = dat$sp, covar = Vphy)   
+# (Random effect for site)
+re.site <- list(1, site = dat$site, covar = diag(nsite))
+
+# Fit model!
+model <- communityPGLMM(Y ~ X, data = dat, family = "binomial", sp = dat$sp, site = dat$site, random.effects = list(re.1, re.2, re.3, re.4), REML = TRUE, verbose = FALSE)
 
